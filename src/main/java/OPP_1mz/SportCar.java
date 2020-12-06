bc7e9602d87200498ec5e75c0686846580dd8fda
Przemek <przem.nowosada@gmail.com>
2020-12-06 22:25:48 +0100

    SportCar dziedziczy z klasy Car

---
+++ src/main/java/OPP_1mz/SportCar.java
@@ -0,0 +1,9 @@
+package OPP_1mz;
+
+public class SportCar extends Car{
+
+    public SportCar(String producer, String model, String color, Integer seatsNumber, Exercise_28_1mz engine) {
+        super(producer, model, color, seatsNumber, engine);
+
+    }
+}
